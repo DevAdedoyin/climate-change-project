9f393c244351bf9d908ff01a9de6db9fa1df7c8e
Adedoyin <59482569+DevAdedoyin@users.noreply.github.com>
2023-04-05 09:25:13 +0100

    barplot for Co2 emission created

--- climate change.py
+++ climate change.py
@@ -8,12 +8,12 @@ Created on Tue Apr  4 22:24:06 2023
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
+import seaborn as sns
 
 # Global Variable
-countries = ["United States", "Canada", "Brazil", "Argentina", 
-             "United Kingdom", "Australia", "Nigeria", "Ghana", 
-             "China", "India"
+countries = ["Argentina", "Australia", "Brazil", "Canada", 
+             "China", "United Kingdom", "Ghana", "India", 
+             "Nigeria", "United States"
              ]
 
 # Function to read the dataset
@@ -50,8 +50,9 @@ def read_dataset(filename):
     # Reset the index of the "country_as_column_dataframe"
     country_as_column_dataframe = country_as_column_dataframe.reset_index()
     
-    # Clean Dataset: Remove unnecessary row and fill NaN with 0                    
-    country_as_column_dataframe = country_as_column_dataframe.drop(1)                       
+    # Clean the transposed dataframe by removing row not needed and 
+    # filling NaN cells with 0
+    #country_as_column_dataframe = country_as_column_dataframe.drop(1)                       
     country_as_column_dataframe = country_as_column_dataframe.fillna(
         0, inplace=False
         )
@@ -60,4 +61,62 @@ def read_dataset(filename):
 
 years_as_column_dataframe, country_as_column_dataframe = \
                                         read_dataset("data.csv")
-#print(years_as_column_dataframe)
+
+
+# This function explore the statistical proper
+def co2_emission():
+    """
+        This function explores the statistical properties of the 
+        "CO2 emissions from liquid fuel consumption (kt)" indicator. 
+        It also plots a graph showing the rate of emission of "C02 from
+        liquid fuel consumption (kt)" for every 10 years from 1966 to 2016.
+    """
+    
+    # Call the read_dataset function and store its two return dataframe in 
+    # new variables
+    years_as_column_dataframe_, country_as_column_dataframe_ = read_dataset(
+                                                                "data.csv")
+
+    # Select from the "Country Name" and "Indicator Name" index, the rows with 
+    # 'CO2 emissions from liquid fuel consumption (kt)' and preferred countries
+    # in the countries list
+    co2_emission_df = years_as_column_dataframe_ \
+        .loc[(years_as_column_dataframe_.index.get_level_values(1) == 
+              'CO2 emissions from liquid fuel consumption (kt)') &
+                 years_as_column_dataframe_\
+                  .index.get_level_values(0).isin(countries)]
+    
+    # Generate an evenly spaced array with length of the countries list        
+    country_column_axis = np.arange(len(countries)) 
+
+    # Create a figure and modify the size of the plot
+    plt.figure(figsize=(10, 6))
+    
+    # Local variables
+    year = 1966
+    increase_year_by = 10
+    decrease_axis = 0.5
+    
+    # This for-loop, loops 6 times and plots the bar graph in group of years 
+    for _range in range(6):
+        year = str(year)
+        plt.bar(country_column_axis - decrease_axis, 
+                co2_emission_df[year], 0.15, label=year)
+        decrease_axis -= 0.15
+        year = int(year) + increase_year_by
+    
+    # Identify ticks for the x-axis and customize the labels
+    plt.xticks(country_column_axis, labels=countries, rotation=60, fontsize=11)
+    plt.yticks(fontsize=11)
+    
+    # Labels for the x and y axis respectively
+    plt.xlabel("Country Name", fontsize=12)
+    plt.ylabel("CO2 emissions (kt)", fontsize=12)
+    
+    # Title of the plot and legend
+    plt.title("CO2 emissions from liquid fuel consumption (kt)", fontsize=14)
+    plt.legend()
+    
+    plt.savefig("Co2 Emission Bar Plot.jpg")
+        
+    plt.show()
